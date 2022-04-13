a4cdd63a2890e85f7036c58c9ba35230d3711ce6
Frederik Bode <thorbensen@gmail.com>
2022-04-13 14:51:41 +0200

    added sitemap plus netlify settings

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,9 +1,18 @@
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = 'https://cars2lease-dev.netlify.app/',
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV
+} = process.env;
+const isNetlifyProduction = NETLIFY_ENV === 'production';
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
+
 module.exports = {
   siteMetadata: {
     title: `CTL Leasing ApS`,
-    description: ``,
+    description: `Vil du realisere din drømmebil til en konkurrencedygtig pris, så kontakt CTL Leasing ApS`,
     author: `@bodeby`,
-    siteUrl: `https://www.cars2lease.dk/`,
+    siteUrl,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -31,7 +40,29 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    },
+    }, {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: siteUrl,
+        sitemap: siteUrl + 'sitemap.xml',
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: '*' }]
+          },
+          'branch-deploy': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null
+          },
+          'deploy-preview': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null
+          }
+        }
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- site/src/components/Feature.js
+++ site/src/components/Feature.js
@@ -5,10 +5,10 @@ const Feature = () => {
     return (
         <section className="text-gray-600 body-font">
             <div className="container px-5 py-24 mx-auto">
-                <h1 className="sm:text-3xl text-2xl font-medium title-font text-center text-gray-900 mb-20">
+                <h2 className="sm:text-3xl text-2xl font-medium title-font text-center text-gray-900 mb-20">
                     CTL Leasing kan hjælpe dig med at gøre drøm til virkelighed
                     <br className="hidden sm:block" />
-                </h1>
+                </h2>
 
                 <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
                     <div className="p-4 md:w-1/3 flex">
@@ -18,7 +18,7 @@ const Feature = () => {
                             </svg>
                         </div>
                         <div className="flex-grow pl-6">
-                            <h2 className="text-gray-900 text-lg title-font font-medium mb-2">Flexleasing</h2>
+                            <h3 className="text-gray-900 text-lg title-font font-medium mb-2">Flexleasing</h3>
                             <p className="leading-relaxed text-base">Flexleasing - tidsbegrænset leasingperiode af bil, hvor der alene beregnes og betales dansk registreringsafgift af køretøjet på grundlag af leasingperiodens varighed. Oversat betyder det, at du sparer den fulde registreringsafgift på bilen, og alene betaler dansk afgift af bilen på månedsbasis.</p>
                             <Link to="/flexleasing" className="mt-3 text-red-500 inline-flex items-center">Se mere om leasing
                                 <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
@@ -37,7 +37,7 @@ const Feature = () => {
                             </svg>
                         </div>
                         <div className="flex-grow pl-6">
-                            <h2 className="text-gray-900 text-lg title-font font-medium mb-2">Hvad koster det?</h2>
+                            <h3 className="text-gray-900 text-lg title-font font-medium mb-2">Hvad koster det?</h3>
                             <p className="leading-relaxed text-base">Når du har valgt at flexlease ved CTL Leasing ApS, udarbejder vi leasingkontrakt og varetager alle opgaver herved indtil vi kan udleveres en nøgle til din bil.
                                 Bilens pris uden registreringsafgift som du betaler, er den samme pris som CTL Leasing ApS afregner til </p>
                             <Link to="/pricing" className="mt-3 text-red-500 inline-flex items-center">Se mere om priser
@@ -56,7 +56,7 @@ const Feature = () => {
                             </svg>
                         </div>
                         <div className="flex-grow pl-6">
-                            <h2 className="text-gray-900 text-lg title-font font-medium mb-2">Se vores udvalg</h2>
+                            <h3 className="text-gray-900 text-lg title-font font-medium mb-2">Se vores udvalg</h3>
                             <p className="leading-relaxed text-base">
                                 Du kan flexlease både en ny eller brugt bil, valget er dit, eneste forudsætning er dog at bilen er uden dansk registreringsafgift. Eksemplerne er vejledende. Pris variationer vil opstå, pga. valg af udstyr, og kilometer.
                             </p>
