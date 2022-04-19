bf089ef4a46dad69367d2b841bce7cdcc798c0fc
Frederik Bode <thorbensen@gmail.com>
2022-04-19 09:53:15 +0200

    updated temp logo

--- site/src/components/Navbar.js
+++ site/src/components/Navbar.js
@@ -1,19 +1,21 @@
 import { Link } from 'gatsby'
 import React from 'react'
+import { AiFillCar } from 'react-icons/ai';
 
-const Navbar = ({path}) => {
+const Navbar = ({ path }) => {
   console.log(path);
 
   const normal = "mr-5 hover:text-gray-900";
   const active = "mr-5 text-red-500 hover:text-gray-900";
 
-  return (    
+  return (
     <header className="text-gray-600 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <Link to='/' className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-red-500 rounded-full" viewBox="0 0 24 24">
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
+          <div className='w-10 h-10 text-white p-2 bg-red-500 rounded-full'>
+            <AiFillCar size={24} className="mx-auto" />
+          </div>
+
           <span className="ml-3 text-xl">CTL Leasing ApS</span>
         </Link>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
@@ -24,7 +26,7 @@ const Navbar = ({path}) => {
           <Link to='/contact' className={path === "/contact" ? active : normal}>Kontakt</Link>
         </nav>
       </div>
-    </header>    
+    </header>
   )
 }
 
